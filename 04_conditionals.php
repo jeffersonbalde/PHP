8a0d7f976246e90b8bbe1b2a9941a379df77fdd7
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-09-03 21:18:53 -0700

    conditionals

---
+++ 04_conditionals.php
@@ -0,0 +1,15 @@
+<?php
+
+    $programming_languages = "PHP";
+
+    // if($programming_languages === "PHP"){
+    //     echo "FUN";
+    // }else {
+    //     echo "HARD";
+    // }
+
+    $feeling = $programming_languages === "PHP" ? "FUN" : "HARD";
+
+    print $feeling;
+ 
+?>
