cf7dd19fe65cd0745112f086bd4cb1513b9f8b71
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-09-16 21:36:25 +0800

    superglobals

---
+++ 09_superglobals.php
@@ -0,0 +1,13 @@
+<?php
+    // $_SERVER
+    echo $_SERVER["DOCUMENT_ROOT"] . '<br>';
+    echo $_SERVER["PHP_SELF"] . '<br>';
+    echo $_SERVER["SERVER_NAME"] . '<br>';
+    echo $_SERVER["REQUEST_METHOD"] . '<br>';
+    // print_r($_SERVER);
+
+    // $_GET
+    echo $_GET["name"];
+
+    
+?>
